5716115f7593f336a20a49113b1bfcb2ebaca27e
David Jarvis <iamdavidjarvis@gmail.com>
2026-08-09 06:25:50 +0800

    [2026-08-09 06:25:50] feat(worktree): add smart commit function for worktrees
    - Add `worktree_smartcommit` function to handle commits across multiple worktrees
    - Set executable permissions for the script
    - Iterate through worktree directories and apply `smartCommit` in each
    - Preserve the main directory context before and after worktree operations

--- src/component/worktreeCommit.sh
+++ src/component/worktreeCommit.sh
@@ -1,2 +1,19 @@
 # component: worktree-commit
 
+function worktree_smartcommit() {
+	main_dir="$PWD"
+	worktree_dir=".worktree"
+	worktree_path="${main_dir}/${worktree_dir}"
+	source_branch="$(git branch --show-current)"
+	readarray -t wt_dirs  < <(fd -d1 -td . -- "${worktree_path}" | sed -r "s|${worktree_path}/(.+)/|\1|g")
+	
+	for wt_dir in "${wt_dirs[@]}"; do {
+		wt_path="${worktree_path}/${wt_dir}"
+		echo "WT: ${wt_dir}"
+		cd "${wt_path}"
+		smartCommit
+		printf '\n\n'
+	} done
+	
+	cd "${main_dir}"
+}
