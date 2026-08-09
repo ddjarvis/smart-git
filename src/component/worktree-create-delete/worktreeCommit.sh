b236b48d538b4e295643204cea85f666c2837353
David Jarvis <iamdavidjarvis@gmail.com>
2026-08-09 12:31:00 +0800

    [2026-08-09 12:31:00] feat(worktree): add colored separators and alias
    - Add ANSI color codes to horizontal rule separators for better visibility
    - Remove trailing newline from `hr` variable to simplify reuse
    - Add `wtsc` alias for `worktree_smartcommit` function

--- src/component/worktree-create-delete/worktreeCommit.sh
+++ src/component/worktree-create-delete/worktreeCommit.sh
@@ -5,17 +5,20 @@ function worktree_smartcommit() {
 	worktree_dir=".worktree"
 	worktree_path="${main_dir}/${worktree_dir}"
 	source_branch="$(git branch --show-current)"
-	hr="$(for (( i = 0; i < COLUMNS; i++ )); do printf '='; done; printf '\n')"
+	hr="$(for (( i = 0; i < COLUMNS; i++ )); do printf '='; done)"
 	readarray -t wt_dirs  < <(fd -d1 -td . -- "${worktree_path}" | sed -r "s|${worktree_path}/(.+)/|\1|g")
 	
 	for wt_dir in "${wt_dirs[@]}"; do {
 		wt_path="${worktree_path}/${wt_dir}"
-		printf '\n%s' "${hr}"
+		col_s='\e[1;38;5;130;48;5;234m'
+		col_e='\e[0m'
+		printf '\n%b%s%b\n' "${col_s}" "${hr}" "${col_e}"
 		printf "\e[1mWorktree:\e[0m %s\n" "${wt_dir}"
 		cd "${wt_path}"
 		smartCommit
-		printf '%s\n\n' "${hr}"
+		printf '%b%s%b\n\n' "${col_s}" "${hr}" "${col_e}"
 	} done
 	
 	cd "${main_dir}"
 }
+alias wtsc='(worktree_smartcommit)'
